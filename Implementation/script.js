6b5cc0c1571a9994fbac5126a4ccdc645258cde1
Siva Kotapati <sivanarasimham.k@gmail.com>
2021-04-19 23:34:10 -0500

    issue fixes

--- Implementation/script.js
+++ Implementation/script.js
@@ -1,23 +1,36 @@
-let tachometer_min_angle = 48.45;
-let tachometer_max_angle = 52.560;
-let increase_decrease_angle_unit = 0.002;
+let tachometer_min_angle = 48.34;
+let tachometer_max_angle = 52.3;
+let increase_decrease_angle_unit = 0.0015;
 let tachometer_move_speed = 500; // in milli secs
 
-let speedometer_min_angle = 48.23;
-let speedometer_max_angle = 52.38;
+let speedometer_min_angle = 79.610;
+let speedometer_max_angle = 90.05;
 let speedometer_decrease_angle_unit = 0.0002;
-let speedometer_move_speed = 400;
+let speedometer_move_speed = 400; // in milli secs
 
+let tachometer_current_angle = 0;
+let speedometer_current_angle = 0;
 let counter = tachometer_min_angle;
 let intervalIds = [];
-let breakState = false;
-let acceleratonState = true;
 
-function accelerateClicked() {
+let initial_travelled_miles = 1000;
+let current_miles = initial_travelled_miles;
+
+let current_fuel_level = 80;
+let minimum_fuel_level_for_warning = 20;
+function accelerateCar() {
+    for(let i=1;i<10;i++)
+    {
+        setTimeout(function () {
+            current_miles += 1
+            updateTotalMiles(current_miles);
+        },i*30000)
+    }
+
     console.log('accelerate clicked');
+
     for (let i = 0; i < 100; i++) {
-        if(i>10)
-        {
+        if (i > 10) {
             break;
         }
         let id = setInterval(function () {
@@ -32,7 +45,7 @@ function accelerateClicked() {
             updateTachometerAngle(new_angle);
             counter += increase_decrease_angle_unit;
             console.log(`${i}: ${counter}`);
-        },tachometer_move_speed);
+        }, tachometer_move_speed);
     }
 }
 
@@ -58,34 +71,135 @@ function breakClicked() {
 
 function stopCar() {
     console.log('car stopping..');
+    slowDownCarSpeed();
+    resetVariables();
 }
 
-function updateTachometerAngle(angle)
-{
-    console.log('updating angle: '+angle);
+function doorOpened() {
+    console.log('door opening');
+    let element = $('#door_opened_img');
+    showOrHideElement(element,"images/Door_Opened.JPG");
+}
+function seatBelt() {
+    console.log('seat belt');
+    let element = $('#seat_belt_img');
+    showOrHideElement(element,"images/Seat_Bealt_Warning.JPG");
+}
+
+function warningAlert() {
+    let element = $('#warning_alert_img');
+    showOrHideElement(element,"images/danger_light.jpg");
+}
+
+function batterLow(){
+    let element = $('#batter_low_img');
+    showOrHideElement(element,"images/Battery_Low.JPG");
+}
+
+function engineTemp(){
+    let element = $('#engine_temp_img');
+    showOrHideElement(element,"images/engine_temperature.jpg");
+}
+
+function oilLeak(){
+    let element = $('#oil_warning_img');
+    showOrHideElement(element,"images/oil_pressure_warning-removebg-preview.jpg");
+}
+
+function updateTotalMiles(current_miles) {
+    document.getElementById('total_travelled').innerHTML = `${current_miles} Miles`;
+}
+
+function showOrHideElement(element, imageName) {
+
+    if($(element).attr('src') == imageName)
+    {
+        $(element).attr('src',"images/dummy_image.JPG")
+        return;          
+    }
+    $(element).attr('src',imageName);
+}
+
+function cruiseControl() {
+    console.log("cruise Controlling");
+    let element = $('#cruise_control_img');
+    showOrHideElement(element,"images/Cruise_Control.png");
+}
+
+function addFuel() {
+    let element = document.getElementById('fuel_percentage');
+    if (element?.style) {
+        let updated_fuel_level = (parseInt(element.style.width.replace('%', '')) + 10);
+        if (updated_fuel_level > 100) {
+            updated_fuel_level = 100;
+        }
+        updateProgressbarClass(element, updated_fuel_level)
+        updateFuelLevelAndText(element, updated_fuel_level);
+    }
+}
+
+function updateFuelLevelAndText(element, updated_fuel_level) {
+    element.style.width = updated_fuel_level + "%";
+    element.innerHTML = element.innerHTML.replace(/[0-9]+/g, updated_fuel_level);
+}
+
+function removeFuel() {
+    let element = document.getElementById('fuel_percentage');
+    if (element?.style) {
+        let updated_fuel_level = (parseInt(element.style.width.replace('%', '')) - 10);
+        if (updated_fuel_level <= 0) {
+            updated_fuel_level = 0;
+        }
+        updateProgressbarClass(element, updated_fuel_level);
+        updateFuelLevelAndText(element, updated_fuel_level);
+    }
+}
+
+function updateProgressbarClass(element, current_fuel_level) {
+    if ($(element).hasClass('progress-bar-info')) {
+        $(element).removeClass('progress-bar-info')
+    }
+
+    if ($(element).removeClass('progress-bar-danger')) {
+        $(element).removeClass('progress-bar-danger')
+    }
+
+    if (current_fuel_level <= minimum_fuel_level_for_warning) {
+        $(element).addClass('progress-bar-danger')
+    }
+}
+
+function updateTachometerAngle(angle) {
+    console.log('updating angle: ' + angle);
     $('#tacho-meter-needle').css('transform', `rotate(${angle}rad)`);
 
     updateSpeedometerAngle(angle - 0.025);
 }
 
 
-function updateSpeedometerAngle(angle){
-    console.log('updating angle: '+angle);
+function updateSpeedometerAngle(angle) {
+    console.log('updating angle: ' + angle);
     $('#speedo-meter-needle').css('transform', `rotate(${angle}rad)`);
 }
 
-function slowDownTachometer(){
+function slowDownTachometer() {
 
 }
 
-function slowDownSpeedometer(){
-    
+function slowDownSpeedometer() {
+
 }
 
-/************************************************************************************************** */
+function resetVariables() {
+    tachometer_current_angle = 0;
+    speedometer_current_angle = 0;
+    counter = tachometer_min_angle;
+    intervalIds = [];
+}
+
 const left_signal_id = "left_signal";
 const right_signal_id = "right_signal";
-const signal_on_color = "green";
+const signal_on_color = "yellow";
 const signal_off_color = "white"
 const singal_on_off_time_in_milliseconds = 900;
 
@@ -100,7 +214,7 @@ let weather_api_url = "https://api.openweathermap.org/data/2.5/forecast?q=Lubboc
 
 
 $(document).ready(function () {
-
+    document.getElementById('total_travelled').innerHTML = `${current_miles} Miles`;
     $("#move_left").click(function () {
         console.log("move left clicked")
         TurnOnLeftOrRightSignal(left_signal_id);
@@ -160,20 +274,24 @@ function TurnOffLeftOrRightSignal(arrowId) {
     }
 }
 
-function playAudio() {
-    console.log("playing audio");;
-}
-
 function getWeatherAndDisplay() {
-    // $.ajax({
-    //     url: weather_api_url + (custom_selected_unit ? custom_selected_unit : default_units), success: function (response) {
-    //         DisplayWeather(response);
-    //     }
-    // })
+    $.ajax({
+        url: weather_api_url + (custom_selected_unit ? custom_selected_unit : default_units), success: function (response) {
+            showWeather(response);
+        }
+    })
     // let json_file_path = (custom_selected_unit == metric_units) ? "sample_weather_response_deg.json" : "sample_weather_response_fah.json"
     // $.getJSON(json_file_path, function (json) {
     //     DisplayWeather(json);
     // });
+}
+function showWeather(response){
+    if (!response) {
+        return;
+    }
+    let data = response.list[0];
+    let temp = data.main.feels_like;
+    document.getElementById('current_temp').innerHTML = `${temp} <sup>°<sup>F`;
 }
 function DisplayWeather(response) {
     if (!response) {
@@ -198,19 +316,7 @@ function DisplayWeather(response) {
 
     $('#notification_section').append(table);
     $('#temperature-table').resizable().draggable();
-
-    // var items = [
-    //     { content: table }, // will default to location (0,0) and 1x1
-    //     { w: 2, content: 'another longer widget!' } // will be placed next at (1,0) and 2x1
-    // ];
-    // var grid = GridStack.init();
-    // grid.load(items);
-    // grid.addWidget(table);
 }
-
-// function formateMonth(input) {
-//     return ("0" + (input + 1)).slice(-2);
-// }
 
 function formateDate(input) {
     return ("0" + input).slice(-2);
